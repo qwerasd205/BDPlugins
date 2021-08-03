46a38b75ad33045fc64d1a94dee313b8aed5fb83
Qwerasd <qwerasd205@users.noreply.github.com>
2021-08-02 21:41:12 -0400

    Add plugin file

---
+++ PinIcon/PinIcon.plugin.js
@@ -0,0 +1,44 @@
+/**
+ * @name PinIcon
+ * @author Qwerasd
+ * @description Add an icon to messages that have been pinned.
+ * @version 0.0.1
+ * @authorId 140188899585687552
+ */
+const MessageModule = BdApi.findModule(m => m.default && m.default.toString && m.default.toString().includes('childrenRepliedMessage'));
+const { TooltipContainer } = BdApi.findModuleByProps("TooltipContainer");
+const Pin = BdApi.findModuleByDisplayName('Pin');
+module.exports = class TestPlugin {
+    start() {
+        BdApi.injectCSS('PinIcon', /*CSS*/ `
+        span.plugin_PinIcon {
+            color: var(--channels-default);
+            margin-inline-start: 3px;
+        }
+        
+        span.plugin_PinIcon > div {
+            display: inline-block;
+        }
+        
+        span.plugin_PinIcon > div > svg {
+            vertical-align: -6px;
+        }`);
+        //@ts-ignore
+        this.unpatch = BdApi.monkeyPatch(MessageModule, 'default', {
+            after: (patchData) => {
+                const isPinned = patchData.thisObject.props.childrenMessageContent.props.message.pinned;
+                if (isPinned) {
+                    patchData.thisObject.props.childrenMessageContent.props.content.push(BdApi.React.createElement("span", { className: "plugin_PinIcon" },
+                        BdApi.React.createElement(TooltipContainer, { text: "Pinned", position: "bottom" },
+                            BdApi.React.createElement(Pin, { width: 18 }))));
+                }
+                return patchData.returnValue;
+            }
+        });
+    }
+    stop() {
+        if (this.unpatch)
+            this.unpatch();
+        BdApi.clearCSS('PinIcon');
+    }
+};
