8c4648ab5e3dc39adec6b0bbe12d039290c1c437
Qwerasd <qwerasd205@users.noreply.github.com>
2022-10-12 22:17:29 -0400

    0.1.0

--- PinIcon/PinIcon.plugin.js
+++ PinIcon/PinIcon.plugin.js
@@ -2,17 +2,29 @@
  * @name PinIcon
  * @author Qwerasd
  * @description Add an icon to messages that have been pinned.
- * @version 0.0.6
+ * @version 0.1.0
  * @authorId 140188899585687552
  * @updateUrl https://betterdiscord.app/gh-redirect?id=421
  */
-const MessageModule = BdApi.findModule(m => m.default && m.default.toString && m.default.toString().includes('childrenRepliedMessage'));
-const { TooltipContainer } = BdApi.findModuleByProps("TooltipContainer");
-const Pin = BdApi.findModuleByDisplayName('Pin');
+// utils/BdApi.ts
+const { React, ReactDOM, Patcher, Webpack, Webpack: { getModule, waitForModule, Filters, Filters: { byProps } }, DOM, Data, UI } = new BdApi('PinIcon');
+// utils/modules/filters.ts
+const hasSubstrings = (str, ...subs) => typeof str === 'string' && subs.every(sub => str.includes(sub));
+const MessageFilter = m => hasSubstrings(m.toString?.(), '"childrenSystemMessage"');
+// utils/utils.ts
+const getKey = (module2, f) => {
+    for (const key in module2) {
+        if (f(module2[key])) return key;
+    }
+};
+// PinIcon/src/PinIcon.plugin.tsx
+const MessageModule = getModule(MessageFilter, { defaultExport: false });
+const Pin = getModule(m => m?.toString?.()?.includes?.('M22 12L12.101 2.10101L10.686'), { searchExports: true });
+// ^^^ This is really stupid, I should just make my own pin SVG.
 module.exports = class PinIcon {
     start() {
-        BdApi.injectCSS('PinIcon', /*CSS*/ `
-        /* Lazy fix for pin icon showing in pinned messages popout '\_(•-•)_/' */
+        DOM.addStyle(`
+        /* Lazy fix for pin icon showing in pinned messages popout '_(•-•)_/' */
         [data-list-item-id^="pins__"] .plugin_PinIcon {
             display: none;
         }
@@ -20,30 +32,39 @@ module.exports = class PinIcon {
         span.plugin_PinIcon {
             color: var(--channels-default);
             margin-inline-start: 3px;
-        }
-        
-        span.plugin_PinIcon > div {
+            position: relative;
             display: inline-block;
+            width: 1em; height: 1em;
         }
         
-        span.plugin_PinIcon > div > svg {
-            vertical-align: -6px;
+        span.plugin_PinIcon > svg {
+            position: absolute;
+            top: -3px;
         }`);
-        BdApi.Patcher.after('PinIcon', MessageModule, 'default', (_, [props]) => {
+        const key = getKey(MessageModule, MessageFilter);
+        if (!key) {
+            console.error("[PinIcon] Can't find key for default in Message module...", MessageModule);
+            return;
+        }
+        Patcher.after(MessageModule, key, (_, [props]) => {
             // catch a whole bunch of annoying things like images in the upload state and stuff like that
-            if (!props.childrenMessageContent?.props?.content?.some)
-                return;
+            if (!props.childrenMessageContent?.props?.content?.some) return;
             const isPinned = props.childrenMessageContent.props.message.pinned;
             const hasIcon = props.childrenMessageContent.props.content.some(c => c.key === 'PinIcon');
             if (isPinned && !hasIcon) {
-                props.childrenMessageContent.props.content.push(BdApi.React.createElement("span", { className: "plugin_PinIcon", key: "PinIcon" },
-                    BdApi.React.createElement(TooltipContainer, { text: "Pinned", position: "bottom", key: "PinIcon" },
-                        BdApi.React.createElement(Pin, { width: 18 }))));
+                props.childrenMessageContent.props.content.push((() => {
+                    const ref = React.useRef(null);
+                    React.useEffect(() => {
+                        if (ref.current == null) return;
+                        UI.createTooltip(ref.current, 'Pinned', { side: 'bottom' });
+                    }, []);
+                    return BdApi.React.createElement('span', { className: 'plugin_PinIcon', key: 'PinIcon', ref }, BdApi.React.createElement(Pin, { width: 18 }));
+                })());
             }
         });
     }
     stop() {
-        BdApi.Patcher.unpatchAll('PinIcon');
-        BdApi.clearCSS('PinIcon');
+        Patcher.unpatchAll();
+        DOM.removeStyle();
     }
 };
--- PinIcon/src/PinIcon.plugin.tsx
+++ PinIcon/src/PinIcon.plugin.tsx
@@ -2,24 +2,29 @@
  * @name PinIcon
  * @author Qwerasd
  * @description Add an icon to messages that have been pinned.
- * @version 0.0.6
+ * @version 0.1.0
  * @authorId 140188899585687552
  * @updateUrl https://betterdiscord.app/gh-redirect?id=421
  */
 
-const MessageModule
-    = BdApi.findModule(m => m.default && m.default.toString && m.default.toString().includes('childrenRepliedMessage'));
+import { React, Patcher, getModule, DOM, UI } from "utils/BdApi";
 
-const {TooltipContainer}: {TooltipContainer: React.ComponentClass<{text: string, position: string}>}
-    = BdApi.findModuleByProps("TooltipContainer");
+import { MessageFilter } from "utils/modules/filters";
+
+import { getKey } from "utils/utils";
+
+const MessageModule
+    = getModule(MessageFilter, { defaultExport: false });
+
 const Pin: React.ComponentClass<{width: number}>
-    = BdApi.findModuleByDisplayName('Pin');
+    = getModule(m => m?.toString?.()?.includes?.('M22 12L12.101 2.10101L10.686'), { searchExports: true });
+// ^^^ This is really stupid, I should just make my own pin SVG.
 
 export = class PinIcon {
     unpatch: Function;
 
     start() {
-        BdApi.injectCSS('PinIcon', /*CSS*/`
+        DOM.addStyle(/*CSS*/`
         /* Lazy fix for pin icon showing in pinned messages popout '\_(•-•)_/' */
         [data-list-item-id^="pins__"] .plugin_PinIcon {
             display: none;
@@ -28,35 +33,48 @@ export = class PinIcon {
         span.plugin_PinIcon {
             color: var(--channels-default);
             margin-inline-start: 3px;
-        }
-        
-        span.plugin_PinIcon > div {
+            position: relative;
             display: inline-block;
+            width: 1em; height: 1em;
         }
         
-        span.plugin_PinIcon > div > svg {
-            vertical-align: -6px;
+        span.plugin_PinIcon > svg {
+            position: absolute;
+            top: -3px;
         }`);
 
-        BdApi.Patcher.after('PinIcon', MessageModule, 'default', (_, [props]) => {
+        const key = getKey(MessageModule, MessageFilter);
+        if (!key) {
+            console.error("[PinIcon] Can't find key for default in Message module...", MessageModule);
+            return;
+        }
+
+        Patcher.after(MessageModule, key, (_, [props]) => {
             // catch a whole bunch of annoying things like images in the upload state and stuff like that
             if (!props.childrenMessageContent?.props?.content?.some) return;
             const isPinned = props.childrenMessageContent.props.message.pinned;
             const hasIcon  = props.childrenMessageContent.props.content.some(c => c.key === 'PinIcon');
             if (isPinned && !hasIcon) {
                 props.childrenMessageContent.props.content.push(
-                    <span className="plugin_PinIcon" key="PinIcon">
-                        <TooltipContainer text="Pinned" position="bottom" key="PinIcon">
-                            <Pin width={18}/>
-                        </TooltipContainer>
-                    </span>
+                    (() => {
+                        const ref = React.useRef(null);
+                        React.useEffect(() => {
+                            if (ref.current == null) return;
+                            UI.createTooltip(ref.current, 'Pinned', { side: 'bottom' });
+                        }, []);
+                        return (
+                            <span className="plugin_PinIcon" key="PinIcon" ref={ref}>
+                                <Pin width={18}/>
+                            </span>
+                        );
+                    })()
                 );
             }
         });
     }
 
     stop() {
-        BdApi.Patcher.unpatchAll('PinIcon');
-        BdApi.clearCSS('PinIcon');
+        Patcher.unpatchAll();
+        DOM.removeStyle();
     }
 }
